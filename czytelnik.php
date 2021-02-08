4e837562d6433f57dd3880c03d227af32658bd50
bibliotekarz <arek@bibliotekarz.com>
2021-02-08 20:28:44 +0100

    fixes in czytelnik.php

--- czytelnik.php
+++ czytelnik.php
@@ -10,61 +10,6 @@ class MyDB extends SQLite3
 }
 
 $user_id_sanitized = 0;
-$costam = [];
-$costam[0]['box_nr'] = "";
-
-if (count($_POST) > 0) {
-    $user_id_sanitized_int = filter_var($_POST["user_id"], FILTER_VALIDATE_INT);
-    $user_id_sanitized_email = filter_var($_POST["user_id"], FILTER_VALIDATE_EMAIL);
-    $user_id_sanitized = $user_id_sanitized_email . $user_id_sanitized_int;
-    // tu jest sanityzowany string
-        echo "<br>".$user_id_sanitized. " user_id_sanitized ".  gettype($user_id_sanitized)."<br><hr>";
-
-
-    if (is_string($user_id_sanitized)) {    
-        $db = new MyDB($dbfile);
-        $stm = $db->prepare('SELECT box_nr FROM "user" where user_id = :user_id ');
-        $stm->bindValue(':user_id', $user_id_sanitized);
-        $score = $stm->execute();
-
-        while ($rowy = $score->fetchArray(1)) {
-            $costam[] = $rowy;
-        }
-
-    } else {
-        $row = "";
-        echo "<h1> row pusty</h1>";
-    }
-
-    foreach ($costam as $v1) {
-        foreach ($v1 as $v2) {
-            echo "wynik $v2 <br>\n";
-
-        }
-    }
-
-// :TODO: dopracować warunki
-    if (isset($costam[0]['box_nr'])) {
-echo " <i> w ifie ma stringa_". $costam[0]['box_nr'] ."_</i>"  ;     
-        // user posiada książkę w skrytce
-        $status = "statusoff33";
-        $status1 = "statuson34";
-        $query_result = " jest wynik z row linia 35";
-    } else {
-        // user nie przeszedł walidacji albo nie ma nic w skrytce
-        
-echo "<i> w elsee niema stringa_". $costam[0]['box_nr'] ."_</i>"   ;     
-        $status = "statuson38";
-        $status1 = "statuson39";
-        $query_result = " brak wyniku z row linia 40";
-    }
-} else {
-    // brak danych w zmiennej post
-    $status = "statuson44";
-    $status1 = "statuson45";
-    $query_result = " brak post linia 46";
-}
-
 $info = [
     "title_info" => "Numer skrytki użytkownika ",
     "no_user_id" => "Nie ma skrytki z zawartością dla Użytkownika o identyfikatorze ",
@@ -74,6 +19,76 @@ $info = [
     "head_info" => "Wprowadź adres email lub nr użytkownika żeby sprawdzić numer swojej skrytki z książkami ",
     "send_button" => "Wyślij",
 ];
+
+$costam = [];
+// $tresc = $info['start_user'];
+$costam[0]['box_nr'] = "";
+
+if (count($_POST) > 0) {
+    $user_id_sanitized_int = filter_var($_POST["user_id"], FILTER_VALIDATE_INT);
+    $user_id_sanitized_email = filter_var($_POST["user_id"], FILTER_VALIDATE_EMAIL);
+    $user_id_sanitized_email = strtolower($user_id_sanitized_email);
+
+    if (is_int($user_id_sanitized_int)) {
+        $sent_condition = "number";
+    } elseif (stristr($user_id_sanitized_email, '@')) {
+        $sent_condition = "email";
+    } else {
+        $sent_condition = "empty";
+    }
+
+    $user_id_sanitized = $user_id_sanitized_email . $user_id_sanitized_int;
+
+    if (is_string($user_id_sanitized)) {
+        $db = new MyDB($dbfile);
+        $stm = $db->prepare('SELECT box_nr FROM "user" where user_id = :user_id ');
+        $stm->bindValue(':user_id', $user_id_sanitized);
+        $score = $stm->execute();
+
+        while ($rowy = $score->fetchArray(1)) {
+            $costam[] = $rowy;
+        }
+    } // else {
+    //    $row = "";
+    // }
+
+
+    foreach ($costam as $v1) {
+        foreach ($v1 as $v2) {
+            echo "wynik $v2 <br>\n";
+        }
+    }
+    
+    // :TODO: dopracować warunki
+    if (is_numeric($costam[0]['box_nr'])) {
+        // user posiada książkę w skrytce
+        $status = "statusoff33";
+        $status1 = "statuson34";
+        foreach ($costam as $v1) {
+            foreach ($v1 as $v2) {
+                $array_loop = $array_loop . "<span class='box-number'>$v2 </span>\n";
+            }
+        }
+        $tresc = "<h3>" . $info['box_found'] . $user_id_sanitized . "</h3>\n" . $array_loop;
+    } elseif (strlen($user_id_sanitized) > 5) { // przejmuje wszystkich
+        // user dobry skrytki brak
+        $status = "statuson38";
+        $status1 = "statuson39";
+        $tresc = "<h3 class='alert'>" . $info['no_user_id'] . $user_id_sanitized . "</h3>";
+    } else {
+        // user nie przeszedł walidacji /puste /litera /mniej cyfr niż 6
+        $status = "statuson38";
+        $status1 = "statuson39";
+        $tresc = "<h3 class='alert'>" . $info['bad_user'] . "</h3>";
+    }
+} else {
+    // brak danych w zmiennej post
+    $status = "statuson83";
+    $status1 = "statuson84";
+    $tresc =  "<h2>" . $info['start_user'] . "</h2>";
+}
+
+
 
 ?>
 <html>
@@ -93,30 +108,7 @@ $info = [
     <main role="main">
         <section class="edycja "> <?php echo " staus section 2 " . $status1; ?>
             <div>
-                <?php
-                if ($user_id_sanitized > 0) {
-                    if (is_numeric($costam[0]['box_nr'])) {
-                        echo "<h3>" . $info['box_found'] . $user_id_sanitized . "</h3>";
-                        foreach ($costam as $v1) {
-                            foreach ($v1 as $v2) {
-                                echo "<span class='box-number'>$v2 </span>\n";
-                            }
-                        }
-                        echo " <br>" . $query_result . " <br>";
-                    } else {
-                        echo "<h3 class='alert'>" . $info['no_user_id'] . $user_id_sanitized . "</h3>";
-                        echo " <br>" . $query_result . " <br>";
-                    }
-                } elseif (count($_POST) <= 0) {
-
-                    echo  "<h2>" . $info['start_user'] . "</h2>";
-                    echo " <br>" . $query_result . " <br>";
-                } else {
-
-                    echo "<h3 class='alert'>" . $info['bad_user'] . "</h3>";
-                    echo " <br>" . $query_result . " <br>";
-                };
-                ?>
+                <?php echo $tresc; ?>
             </div>
         </section>
         <section class="edycja"><?php echo " status sekcja 1 " . $status; ?>
