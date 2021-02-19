24eff1dfd291652bd3f0e757a29a6eaedc1f78c3
bibliotekarz <arek@bibliotekarz.com>
2021-02-19 16:28:14 +0100

    added password encryption in the database

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 <?php
 require 'config.php';
 session_start();
-$message = "";
-if (count($_POST) > 0) {
 
+if (count($_POST) > 0) {
+    $user_id_sanitized = filter_var($_POST["user_name"], FILTER_VALIDATE_EMAIL);
 
     class MyDB extends SQLite3
     {
@@ -12,31 +12,28 @@ if (count($_POST) > 0) {
             $this->open($dbfile);
         }
     }
-    // TODO: sanityzacja loginu 
 
     $db = new MyDB($dbfile);
-    $stm = $db->prepare("SELECT * FROM librarian WHERE librarian_name = '" . $_POST["user_name"] . "' and librarian_pass = '" . $_POST["password"] . "'");
-    // TODO:        $stm->bindValue(':user_id', $user_id_sanitized);
+    $stm = $db->prepare("SELECT * FROM librarian WHERE librarian_name = :user_id");
+    $stm->bindValue(':user_id', $user_id_sanitized);
     $score = $stm->execute();
     $row = $score->fetchArray(1);
-    if (is_array($row)) {
+
+// TODO: wywalić Notice: Trying to access array offset on value of type bool
+    if (password_verify($_POST["password"], $row['librarian_pass'])) {
         $_SESSION["id"] = $row['librarian_id'];
         $_SESSION["name"] = $row['librarian_name'];
+        $_SESSION["pass"] = $row['librarian_pass'];
     } else {
         $message = "<h3 class='alert'>" . $info['login_alert'] . "</h3>";
     }
 }
 if (isset($_SESSION["id"])) {
+
     header("Location:admin.php");
 }
-?>
-<html>
 
-<head>
-    <title><?php echo $info['login_title']; ?></title>
-
-    <link rel="stylesheet" type="text/css" href="./style.css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+echo $page_head . "\n\t\t<title>" . $info['login_title']; ?></title>
 </head>
 
 <body>
@@ -44,13 +41,12 @@ if (isset($_SESSION["id"])) {
         <h3 class=""><?php echo $info['login_title']; ?></h3>
         <section class="edycja">
             <form name="login" method="post" action="" class="">
-                    <?php echo ($message != "") ? $message : ''; ?>
+                <?php echo ($message != "") ? $message : ''; ?>
 
                 <div class="flekser">
                     <label for="user_name"><?php echo $info['login_name']; ?></label>
                     <input type="text" name="user_name">
                 </div>
-                <br>
                 <div class="flekser">
                     <label for="password"><?php echo $info['login_pass']; ?></label>
                     <input type="password" name="password">
