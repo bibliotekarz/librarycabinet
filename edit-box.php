c1dd9fad20f0b74e722a9d184f1e037532d19fc1
bibliotekarz <arek@bibliotekarz.com>
2021-03-12 20:11:35 +0100

    set a condition for insert_box

--- edit-box.php
+++ edit-box.php
@@ -1,14 +1,14 @@
 <?php
 require 'config.php';
 session_start();
-
+/*
 print_r($_POST);
 echo "<br> powyżej post <bR><bR><bR>";
 print_r($_GET);
 echo "<br> powyżej get <bR><bR><bR>";
 print_r($_SESSION);
 echo "<br> powyżej session <bR><bR><bR>";
-
+*/
 
 ///////////////////////////////
 
@@ -41,18 +41,17 @@ while ($row = $score->fetchArray(1)) {
     $unit_address = $row['unit_address'];
 };
 
-$end_date = $_POST [end-date];
-$title = $_POST [title];
-$secret_code = $_POST [secret-code];
-$id_user = $_POST [id-user];
-$box = $_POST [box];
-$id = $_POST [id];
+
+$end_date = $_POST['end_date'];
+$title = $_POST['title'];
+$secret_code = $_POST['secret_code'];
+$id_user = $_POST['id_user'];
+$box = $_POST['box'];
+$id = $_POST['id'];
 
 
 
 function update_box($db, $end_date, $title, $secret_code, $id_user, $box, $id){
-
-// TODO: not working 
         $stm = $db->prepare("UPDATE user set date_insertion = :end_date, title = :title, access_code = :secret_code, user_id = :id_user where box_nr = :box and unit_id = :id");
         $stm->bindValue(':end_date', $end_date);
         $stm->bindValue(':title', $title);
@@ -61,6 +60,18 @@ function update_box($db, $end_date, $title, $secret_code, $id_user, $box, $id){
         $stm->bindValue(':box', $box);
         $stm->bindValue(':id', $id);
         $stm->execute();
+
+}
+
+function insert_box($db, $end_date, $title, $secret_code, $id_user, $box, $id){
+    $stm = $db->prepare("INSERT INTO user (date_insertion, title, access_code, user_id, box_nr, unit_id) VALUES (:end_date, :title, :secret_code, 'Texas', :id_user, :box,:id)");
+    $stm->bindValue(':end_date', $end_date);
+    $stm->bindValue(':title', $title);
+    $stm->bindValue(':secret_code', $secret_code);
+    $stm->bindValue(':id_user', $id_user);
+    $stm->bindValue(':box', $box);
+    $stm->bindValue(':id', $id);
+    $stm->execute();
 }
 
 function selected_box($db, $selected_unit, $selected_box){
@@ -79,12 +90,15 @@ function selected_box($db, $selected_unit, $selected_box){
         $user_id = $row_box['user_id'];
     };
     $box_data = array($title, $access_code, $date_insertion, $user_id);
-
-
     return $box_data;
 }
 
 $box_info = selected_box($db, $selected_unit, $selected_box);
+
+if (isset($_SESSION["name"]) && isset($box) && isset($id) ){
+update_box($db, $end_date, $title, $secret_code, $id_user, $box, $id);
+}
+
 
 /////////////////////
 
@@ -115,19 +129,19 @@ echo $page_head . "\n\t\t<title>" . $info['machine_title']; ?></title>
                     <div class="flekser"><span class="box-number"><?php echo $selected_box; ?></span>
                         <!-- div class="panel-checkboxes"><label>skrytka pełna<br><input type="checkbox"></label></div -->
                         <div class="panel-checkboxes"><input type="reset"  id="" value="przywróć dane"><br>
-                        <input type="button" id="clear_fields" value="wyczyść pola"></div>
+                        <input type="button" id="clear_fields" value="Opróżnij skrytkę"></div>
                     </div>
                     <div class="descriptive-container">
-                        <div class="descriptive-container-end"><label for="title">Tytuł</label></div>
-                        <div class=""><input class="" type="text" name="title" id="box-title" value="<?php echo $box_info[0]; ?>">
-                        </div>
-                        <div class="descriptive-container-end"><label for="secret-code" class="">kod do skrytki</label></div>
-                        <div class=""><input class="" id="box-secret-code" name="secret-code" value="<?php echo $box_info[1]; ?>" type="number"></div>
-                        <div class="descriptive-container-end"><label for="end-date" class="">data opróżnienia</label></div>
-                        <div class=""><input class="" id="box-end-date" name="end-date" value="<?php echo $box_info[2]; ?>" type="date"></div>
-                        <div class="descriptive-container-end"><label for="title" class="">id czytelnika</label></div>
-                        <div class=""><input class="" id="box-id-user" name="id-user" value="<?php echo $box_info[3]; ?>"></div>
+                        <div class="descriptive-container-end"><label for="title" class="">Tytuł</label></div>                
+                        <div class=""><input class="" id="box_title" name="title" value="<?php echo $box_info[0]; ?>" type="text"></div>
+                        <div class="descriptive-container-end"><label for="secret_code" class="">kod do skrytki</label></div> 
+                        <div class=""><input class="" id="box_secret_code" name="secret_code" value="<?php echo $box_info[1]; ?>" type="number"></div>
+                        <div class="descriptive_container_end"><label for="end_date" class="">data opróżnienia 1</label></div>  
+                        <div class=""><input class="" id="box_end_date" name="end_date" value="<?php echo $box_info[2]; ?>" type="date"></div>
+                        <div class="descriptive-container-end"><label for="id_user" class="">id czytelnika</label></div>      
+                        <div class=""><input class="" id="box_id_user" name="id_user" value="<?php echo $box_info[3]; ?>"></div>
                         <input type="hidden" value="<?php echo $selected_box; ?>" name="box">
+                        <input type="hidden" value="<?php echo $selected_unit; ?>" name="id">
                         <input type="hidden" value="<?php echo $selected_unit; ?>" name="id">
                     </div>
                 </div>
